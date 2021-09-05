924f3a4879bf6740144e5b05890d5ad23d69f9a2
Brian J. Tarricone <brian@tarricone.org>
2021-09-04 22:14:20 -0700

    Fix incorrect app name in license headers

--- espmonitor/src/main.rs
+++ espmonitor/src/main.rs
@@ -1,19 +1,19 @@
 // Copyright 2021 Brian J. Tarricone <brian@tarricone.org>
 //
-// This file is part of ESPFlash.
+// This file is part of ESPMonitor.
 //
-// ESPFLash is free software: you can redistribute it and/or modify
+// ESPMonitor is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// ESPFlash is distributed in the hope that it will be useful,
+// ESPMonitor is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with ESPFlash.  If not, see <https://www.gnu.org/licenses/>.
+// along with ESPMonitor.  If not, see <https://www.gnu.org/licenses/>.
 
 use espmonitor::{AppArgs, Chip, Framework, run};
 use pico_args::Arguments;
