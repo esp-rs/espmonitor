9c1dadd9e17fa1a0ff2c2b6ade58f3ae113f9a84
Brian J. Tarricone <brian@tarricone.org>
2021-09-04 22:10:03 -0700

    Query crates.io on release to ensure espflash has been published

--- release.sh
+++ release.sh
@@ -3,14 +3,36 @@
 set -e
 
 token="$1"
+new_version=$(awk '/^version = /{ print $3; exit; }' espmonitor/Cargo.toml | sed -e 's/"//g')
+
+[ "token" ] || {
+    echo "Usage: $0 CRATES_IO_TOKEN" >&2
+    exit 1
+}
+[ "$new_version" ] || {
+    echo "Unable to determine new version" >&2
+    exit 1
+}
 
 echo "Publishing espmonitor..."
 pushd espmonitor
 cargo publish --token $token
 popd
 
-echo "Waiting for espmonitor to become available on crates.io"
-sleep 60
+echo "Waiting for espmonitor $new_version to become available on crates.io"
+wait=60
+max_version=''
+while [ $wait -gt 0 -a "$max_version" != "$new_version" ]; do
+    sleep 1
+    max_version=$(curl -s -f https://crates.io/api/v1/crates/espmonitor | jq -r .crate.max_version)
+    wait=$((wait-1))
+done
+if [ "$max_version" == "$new_version" ]; then
+    echo "New version $max_version published"
+else
+    echo "New version $new_version failed to show up; latest version is currently $max_version" >&2
+    exit 1
+fi
 
 echo "Publishing cargo-espmonitor..."
 pushd cargo-espmonitor
