8edad77b9c0c24e62c4111cef5abcc783ca4196a
Brian J. Tarricone <brian@tarricone.org>
2021-09-24 18:14:08 -0700

    Write prepare-release script and update release instructions
    I just spent over an hour trying to get cargo-release to do what I want,
    but it is incredibly confusing to use, and for some reason does not
    properly support the simplest release method: release all crates in the
    workspace with the same new version.

--- RELEASING.md
+++ RELEASING.md
@@ -1,18 +1,9 @@
 # Releasing this crate
 
-1. Bump the version in `espmonitor/Cargo.toml` and
-   `cargo-espmonitor/Cargo.toml`.  Also bump the `espmonitor` dependency
-   version in `cargo-espmonitor/Cargo.toml`.
-2. Run `cargo build` to update `Cargo.lock'.`
-3. Commit these changes.
-4. Tag the commit with the new version (prefixed with a "v"; e.g.
-   `v0.3.1`).  If you are into that sort of thing, sign the tag with
-   your GPG key.
-5. Push the version bump commit and the tag.
-6. Go to the GitHub releases page for this repo, and create a release
-   against the tag you just pushed.
-7. Once the release is created, GitHub Actions will build the new
+1. Run `./prepare-release.sh NEW_VERSION` (substituting the new desired
+   version for `NEW_VERSION`).
+2. Go to the GitHub releases page for this repo.  There should be a
+   draft release waiting.  Edit it, set the title to the new version,
+   and point it to the new tag that was just pushed.
+3. Once the release is created, GitHub Actions will build the new
    release and publish it to crates.io.
-
-Alternative: if you don't GPG-sign your tag, you can also just create
-the tag as a part of creating the GH release.
---
+++ prepare-release.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+new_version="$1"
+
+[ "$new_version" ] || {
+    echo "Usage: $0 NEW_VERSION" >&2
+    exit 1
+}
+
+toml_set() {
+    local file="$1"
+    local query="$2"
+    local value="$3"
+
+    [ "$value" ] || {
+        echo "Usage: toml_set FILE QUERY VALUE" >&2
+        return 1
+    }
+
+    toml set "$file" "$query" "$value" > "$file.new"
+    mv "$file.new" "$file"
+}
+
+set_version() {
+    local version="$1"
+
+    [ "$version" ] || {
+        echo "Usage: set_version VERSION" >&2
+        return 1
+    }
+
+    toml_set espmonitor/Cargo.toml package.version "$version"
+    toml_set cargo-espmonitor/Cargo.toml dependencies.espmonitor.version "^$version"
+    toml_set cargo-espmonitor/Cargo.toml package.version "$version"
+}
+
+# The does not work on alpha/rc/etc. versions, just x.y.z versions
+new_patch_version() {
+    local old_version="$1"
+
+    [ "$old_version" ] || {
+        echo "Usage: new_patch_version OLD_VERSION" >&2
+        exit 1
+    }
+
+    echo "$old_version" | awk -F. '{ ++$3; printf "%d.%d.%d", $1, $2, $3 }'
+}
+
+if ! type -p toml; then
+    echo "Installing toml-cli..."
+    cargo install toml-cli
+fi
+
+set_version "$new_version"
+cargo update --package espmonitor
+git commit -a -m "Bump to $new_version"
+git tag -s "v$new_version" -m "$new_version"
+
+dev_version=$(new_patch_version "$new_version")-alpha.1
+set_version "$dev_version"
+cargo update --package espmonitor
+git commit -a -m "Bump to $dev_version"
+
+git push --tags origin "$(git rev-parse --abbrev-ref HEAD)"
